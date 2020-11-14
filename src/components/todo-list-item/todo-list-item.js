28a7ed16941fd919ea8f9953e49916173ffa5ba1
The3Legend <durdvlad@gmail.com>
2020-11-14 18:19:55 +0200

    add event done

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -9,29 +9,50 @@ export default class TodoListItem extends Component {
   //      console.log(`done:${this.props.label}`);
   //   };
   // }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     done: false,
+  //   };
+  // }
+  // Синтаксис полей класа
+  state = {
+    done: false,
+    important: false,
+  };
+
   onLabelClick = () => {
-    console.log(`done:${this.props.label}`);
-  }
+    this.setState(({done}) => {
+      return { done: !done };
+    });
+  };
+  onMarkImportant = () => {
+    this.setState(({important}) => {
+      return { important: !important };
+    });
+  };
   render() {
-    const { label, important = false } = this.props;
-    const style = {
-      color: important ? "steelblue" : "black",
-      fontWeight: important ? "bold" : "normal",
-    };
+    const { label } = this.props;
+    const { done, important } = this.state;
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += " done";
+    }
+
+    if (important) {
+      classNames += " important";
+    }
 
     return (
-      <span className="todo-list-item">
-        <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={this.onLabelClick}
-        >
+      <span className={classNames}>
+        <span className="todo-list-item-label" onClick={this.onLabelClick}>
           {label}
         </span>
 
         <button
           type="button"
           className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
         >
           <i className="fa fa-exclamation" />
         </button>
